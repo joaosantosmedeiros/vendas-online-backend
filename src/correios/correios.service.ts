be4e9a062642fd3470b4c0f603db8807b7034262
joaosantosmedeiros <jopesame@gmail.com>
2023-09-18 17:27:15 -0300

    feat: add correios module

---
+++ src/correios/correios.service.ts
@@ -0,0 +1,31 @@
+import { HttpService } from '@nestjs/axios';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { AxiosError, AxiosResponse } from 'axios';
+
+@Injectable()
+export class CorreiosService {
+  readonly URL_CORREIOS = process.env.URL_CEP_CORREIOS;
+  constructor(private readonly httpService: HttpService) {}
+
+  async findAddressByCep(cep: string): Promise<AxiosResponse<any>> {
+    return this.httpService.axiosRef
+      .get(this.URL_CORREIOS.replace('{CEP}', cep))
+      .then((result) => {
+        if (result.data.erro === true) {
+          throw new NotFoundException('CEP not found.');
+        }
+        return result.data;
+      })
+      .catch((error: AxiosError) => {
+        console.log(error);
+        throw new BadRequestException(
+          'Error in connection request',
+          error.message,
+        );
+      });
+  }
+}
